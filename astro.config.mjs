76c7630d0441d88b460092eca4a6711b2cb46320
IlijaBertle <ilijabertle@proton.me>
2026-07-17 17:22:29 +0200

    Aktualisieren von astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wirschaft.de',
+  site: 'https://wirschaft.eu',
   integrations: [react(), sitemap()]
 });
